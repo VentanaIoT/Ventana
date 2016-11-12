3874402c7d7d21a70874d36f6a8865105a346847
Johan Ospina <johanos@bu.edu>
2016-11-12 18:01:34 -0500

    got artwork to update

--- Ventana/Assets/Ventana/Scenes/AlbumSlabTextureController.cs
+++ Ventana/Assets/Ventana/Scenes/AlbumSlabTextureController.cs
@@ -2,38 +2,53 @@
 using System.Collections;
 
 public class AlbumSlabTextureController : MonoBehaviour {
-    private string newURL = "http://is5.mzstatic.com/image/thumb/Music71/v4/c0/39/b4/c039b43b-11cf-2a33-6a77-2323fb4c2b9f/source/100000x100000-999.jpg";
-
-
-
+    public string newURL;
+    public Texture oldTexture;
     void Start() {
+        newURL = "http://is5.mzstatic.com/image/thumb/Music3/v4/47/97/af/4797af7e-24c9-7428-ac64-5b5f35eba51e/source/100000x100000-999.jpg";
         WWW www = new WWW(newURL);
         StartCoroutine(WaitForRequest(www));
+
+        InvokeRepeating("requestAlbum", 1.0f, 0.25f);
     }
 
     // Update is called once per frame
-    void Update () {
-	
-	}
+    void Update() {
+
+
+    }
+
+    void requestAlbum() {
+        gameObject.SendMessageUpwards("makeAPIRequest", "albumArt");
+        Debug.Log("Requesting the Album Art...");
+
+    }
 
     IEnumerator WaitForRequest(WWW www) {
         yield return www;
 
         if ( www.error == null ) {
             Debug.Log("WWW OK!");
+            GetComponent<Renderer>().material.mainTexture = null;
+            //www.LoadImageIntoTexture(renderer.material.mainTexture);
             Renderer renderer = GetComponent<Renderer>();
-            Texture2D image = www.texture;
+            renderer.material.mainTexture = www.texture;
+            Debug.Log(www.texture);
 
-            renderer.material.mainTexture = image;
 
         } else {
-
+            Debug.Log("Not changing texture");
         }
     }
 
     void OnURLSent(string url) {
         Debug.Log(url);
-        WWW www = new WWW(newURL);
-        StartCoroutine(WaitForRequest(www));
+        if ( newURL != url ) {
+            Debug.Log("Im getting changed");
+            WWW www = new WWW(url);
+            newURL = url;
+            StartCoroutine(WaitForRequest(www));
+
+        }
     }
 }
