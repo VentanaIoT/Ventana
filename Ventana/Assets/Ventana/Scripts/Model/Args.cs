d7f7c3eb94dd3fa1793627486bef3e48b53c89a6
Johan Ospina <johanos@bu.edu>
2017-01-29 16:18:16 -0500

    Beautifying things

--- Ventana/Assets/Ventana/Scripts/Model/Args.cs
+++ Ventana/Assets/Ventana/Scripts/Model/Args.cs
@@ -7,4 +7,5 @@ public class Args {
     public static string PREFAB_LOCATION = "Ventana/Prefabs/"; //in Resources Folder
     public static string HOLOHUB_ADDRESS = "192.168.0.100:5000";
     public static string VENTANA_MARK_CONFIG_FILE_LOCATION = "Assets/Resources/Ventana/VentanaConfig.json";
+    public static string VENTANA_DATA_SET_NAME = "QCAR\\VentanaTargets.xml";
 }
