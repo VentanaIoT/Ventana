0a4bea0794e9b16fbab7b15a9c964db250d2dff5
Johan Ospina <johanos@bu.edu>
2017-01-31 23:08:48 -0500

    adding elements to make the UI responsive

--- Ventana/Assets/Ventana/Scripts/MusicButtonHandler.cs
+++ Ventana/Assets/Ventana/Scripts/MusicButtonHandler.cs
@@ -2,17 +2,30 @@
 using UnityEngine;
 using System;
 
-public class MusicButtonHandler : MonoBehaviour, IInputClickHandler {
+public class MusicButtonHandler : MonoBehaviour, IInputClickHandler, IFocusable {
+    public AudioClip clickSound;
+    private AudioSource source;
+    public Material highlightButtonMaterial;
+    public Material normalButtonMaterial;
+
+    public void OnFocusEnter() {
+        gameObject.GetComponent<Renderer>().material = highlightButtonMaterial;
+    }
+
+    public void OnFocusExit() {
+        gameObject.GetComponent<Renderer>().material = normalButtonMaterial;
+    }
+
     public void OnInputClicked(InputEventData eventData) {
         Debug.Log("Clicked " + gameObject.name);
         gameObject.SendMessageUpwards("makeAPIRequest", gameObject.name);
+        source.PlayOneShot(clickSound, 1F);
+
     }
-
-
-
 
     // Use this for initialization
     void Start () {
+        source = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
