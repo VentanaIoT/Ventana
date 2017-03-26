d0f2e89ea6bcd00ce6eea3319390458cbc6ccbfc
tgauth <tgauth@bu.edu>
2017-03-26 15:50:42 -0400

    tap to place mode coming in hot

---
+++ Ventana/Assets/Ventana/Scripts/Interaction/Placement/ddScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ddScript : MonoBehaviour, IInputClickHandler {
+
+	// Use this for initialization
+	void Start () {
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("button pressed");
+        gameObject.SendMessageUpwards("ddButtonClicked", gameObject.name);
+    }
+}
---
+++ Ventana/Assets/Ventana/Scripts/Interaction/Placement/scalingHandler.cs
@@ -0,0 +1,53 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class scalingHandler : MonoBehaviour, IManipulationHandler
+{
+
+    [SerializeField]
+    bool resizingEnabled = true;
+    void Start()
+    {
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+    }
+
+    public void SetResizing(bool enabled)
+    {
+        resizingEnabled = enabled;
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        
+        InputManager.Instance.PushModalInputHandler(gameObject);
+
+        // send message to ttpMode.cs
+        gameObject.SendMessageUpwards("scaleStarted");
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        if (resizingEnabled)
+            Resize(eventData.CumulativeDelta);
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+    void Resize(Vector3 newScale)
+    {
+        // send data to ttpMode.cs once the manipulation gesture is updated
+        Debug.Log("bubbling to scalebuttonclicked" + newScale);
+        gameObject.SendMessageUpwards("scaleButtonClicked", newScale);
+    }
+}
